f11ba98d8df18c87455dfd540543ab097bb39ddb
NatanaelBarrionuevo <nato.barrionuevo@gmail.com>
2023-11-15 00:07:30 -0300

    15/11/23 Back y Front

---
+++ TindaMusica/TiendaMusicaBack/Enitidades/TipoProductos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaMusicaBack.Enitidades
+{
+    public class TipoProductos
+    {
+		private int id;
+		private string tipo;
+
+		public string Tipo
+		{
+			get { return tipo; }
+			set { tipo = value; }
+		}
+
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+        public TipoProductos(int id, string tipo)
+        {
+			Id = id;
+			Tipo = tipo;
+        }
+		public TipoProductos()
+		{
+
+		}
+    }
+}
